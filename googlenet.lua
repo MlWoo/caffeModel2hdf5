27e05b4f1314d60bb285a639632213d9d84c505d
MlWoo <menglin.wu@intel.com>
2016-10-16 22:18:37 -0400

    read params and input

--- googlenet.lua
+++ googlenet.lua
@@ -6,34 +6,34 @@ torch.setdefaulttensortype('torch.FloatTensor')
 function InceptionModule(name, inplane, outplane_a1x1, outplane_b3x3_reduce, outplane_b3x3, outplane_c5x5_reduce, outplane_c5x5, outplane_pool_proj)
   local a = nn.Sequential()
   local a1x1 = nn.SpatialConvolution(inplane, outplane_a1x1, 1, 1, 1, 1, 0, 0)
-  a1x1.name = name .. '/1x1'
+  a1x1.name = name .. '_1x1'
   a:add(a1x1)
   a:add(nn.ReLU(true))
 
   local b = nn.Sequential()
   local b3x3_reduce = nn.SpatialConvolution(inplane, outplane_b3x3_reduce, 1, 1, 1, 1, 0, 0)
-  b3x3_reduce.name = name .. '/3x3_reduce'
+  b3x3_reduce.name = name .. '_3x3_reduce'
   b:add(b3x3_reduce)
   b:add(nn.ReLU(true))
   local b3x3 = nn.SpatialConvolution(outplane_b3x3_reduce, outplane_b3x3, 3, 3, 1, 1, 1, 1)
-  b3x3.name = name .. '/3x3'
+  b3x3.name = name .. '_3x3'
   b:add(b3x3)
   b:add(nn.ReLU(true))
 
   local c = nn.Sequential()
   local c5x5_reduce = nn.SpatialConvolution(inplane, outplane_c5x5_reduce, 1, 1, 1, 1, 0, 0)
-  c5x5_reduce.name = name .. '/5x5_reduce'
+  c5x5_reduce.name = name .. '_5x5_reduce'
   c:add(c5x5_reduce)
   c:add(nn.ReLU(true))
   local c5x5 = nn.SpatialConvolution(outplane_c5x5_reduce, outplane_c5x5, 5, 5, 1, 1, 2, 2)
-  c5x5.name = name .. '/5x5'
+  c5x5.name = name .. '_5x5'
   c:add(c5x5)
   c:add(nn.ReLU(true))
 
   local d = nn.Sequential()
   d:add(nn.SpatialMaxPooling(3, 3, 1, 1, 1, 1))
   local d_pool_proj = nn.SpatialConvolution(inplane, outplane_pool_proj, 1, 1, 1, 1, 0, 0)
-  d_pool_proj.name = name .. '/pool_proj'
+  d_pool_proj.name = name .. '_pool_proj'
   d:add(d_pool_proj)
   d:add(nn.ReLU(true))
 
@@ -44,18 +44,18 @@ end
 local model = nn.Sequential()
 
 local conv1_7x7_s2 = nn.SpatialConvolution(3, 64, 7, 7, 2, 2, 3, 3)
-conv1_7x7_s2.name = 'conv1/7x7_s2'
+conv1_7x7_s2.name = 'conv1_7x7_s2'
 model:add(conv1_7x7_s2)
 model:add(nn.ReLU(true))
 model:add(nn.SpatialMaxPooling(3, 3, 2, 2):ceil())
 model:add(nn.SpatialCrossMapLRN(5, 0.0001, 0.75, 1))
 
 local conv_3x3_reduce = nn.SpatialConvolution(64, 64, 1, 1, 1, 1, 0, 0)
-conv_3x3_reduce.name = 'conv2/3x3_reduce'
+conv_3x3_reduce.name = 'conv2_3x3_reduce'
 model:add(conv_3x3_reduce)
 model:add(nn.ReLU(true))
 local conv_3x3 = nn.SpatialConvolution(64, 192, 3, 3, 1, 1, 1, 1)
-conv_3x3.name = 'conv2/3x3'
+conv_3x3.name = 'conv2_3x3'
 model:add(conv_3x3)
 model:add(nn.ReLU(true))
 model:add(nn.SpatialCrossMapLRN(5, 0.0001, 0.75, 1))
@@ -93,7 +93,7 @@ model:add(nn.View(-1, 1024))
 --model:add(nn.Dropout(0.4))
 
 local classifier3 = nn.Linear(1024, 1000)
-classifier3.name = 'loss3/classifier'
+classifier3.name = 'loss3_classifier'
 model:add(classifier3)
 
 -----------------------------------------------------------------
@@ -106,19 +106,19 @@ local af_softmax1 = nn.Sequential()
 af_softmax1:add(nn.SpatialAveragePooling(5, 5, 3, 3))
 
 local conv1_1x1_s1 = nn.SpatialConvolution(512, 128, 1, 1)
-conv1_1x1_s1.name = 'loss1/conv'
+conv1_1x1_s1.name = 'loss1_conv'
 af_softmax1:add(conv1_1x1_s1)
 af_softmax1:add(nn.ReLU(true))
 af_softmax1:add(nn.View(128*4*4):setNumInputDims(3))
 
 local linear_sftmax1 = nn.Linear(128*4*4, 1024)
-linear_sftmax1.name = 'loss1/fc'
+linear_sftmax1.name = 'loss1_fc'
 af_softmax1:add(linear_sftmax1)
 af_softmax1:add(nn.ReLU(true))
 --bf_sftmax1:add(nn.Dropout(0.7))
 
 local classifier1 = nn.Linear(1024, 1000)
-classifier1.name = 'loss1/classifier'
+classifier1.name = 'loss1_classifier'
 af_softmax1:add(classifier1)
 -------------------------------------------------------------------
 
@@ -131,28 +131,31 @@ local af_softmax2 = nn.Sequential()
 af_softmax2:add(nn.SpatialAveragePooling(5, 5, 3, 3))
 
 local conv1_1x1_s1_copy = nn.SpatialConvolution(528, 128, 1, 1)
-conv1_1x1_s1_copy.name = 'loss2/conv'
+conv1_1x1_s1_copy.name = 'loss2_conv'
 af_softmax2:add(conv1_1x1_s1_copy)
 af_softmax2:add(nn.ReLU(true))
 
 af_softmax2:add(nn.View(128*4*4):setNumInputDims(3))
 
 local linear_sftmax2 = nn.Linear(128*4*4, 1024)
-linear_sftmax2.name = 'loss2/fc'
+linear_sftmax2.name = 'loss2_fc'
 af_softmax2:add(linear_sftmax2)
 af_softmax2:add(nn.ReLU(true))
 --bf_sftmax2:add(nn.Dropout(0.7))
 
 local classifier2 = nn.Linear(1024, 1000)
-classifier2.name = 'loss2/classifier'
+classifier2.name = 'loss2_classifier'
 af_softmax2:add(classifier2)
 ------------------------------------------------------------------
 
 
-local paramsFile = hdf5.open('/root/mlwu/googlenet.hdf5', 'r')
-local moduleQueue = {  af_softmax1, af_softmax2}
---local moduleQueue = { model, af_softmax1, af_softmax2 }
+local paramsFile = hdf5.open('train_val.hdf5', 'r')
+--local moduleQueue = {  af_softmax1, af_softmax2}
+local moduleQueue = { model, af_softmax1, af_softmax2 }
 local touchedLayers = { }
+local inputData = paramsFile:read('InputLayer'):all()
+
+
 for k1, v1 in ipairs(moduleQueue) do
   if v1.modules then
     for k2, v2 in ipairs(v1.modules) do
@@ -161,110 +164,58 @@ for k1, v1 in ipairs(moduleQueue) do
   end
 
   if v1.name then
-    print(v1.name)
-    touchedLayers[v1.name] = true
-    local layer = paramsFile:read(v1.name):all()
-    if layer['000'] then
-      v1.weight:copy(layer['000'])
+     touchedLayers[v1.name] = true
+     weight_name = v1.name .. "___" .. "weight"
 
-    else
-      print(v1.name .. ' has no weight')
-    end
-    if layer['001'] then
-      v1.bias:copy(layer['001'])
-    else
-      print(v1.name .. ' has no bias')
-    end
-  end
+     print(weight_name)
+     
+     local layer_weight = paramsFile:read(weight_name):all()
+     if layer_weight then
+         v1.weight:copy(layer_weight)
+     
+         bias_name = v1.name .. "___" .. "bias"
+         print(bias_name)
+         local layer_bias = paramsFile:read(bias_name):all()
+         if layer_bias then
+           v1.bias:copy(layer_bias)
+         else
+           print(v1.name .. ' has no bias')
+         end
+     end
+   end
 end
 
 paramsFile:close()
-
+print('  ')
+print('  ')
+print(inputData:size())
+print('  ')
 print('  ')
 print('before inception')
 bf_inception = bf_inception:float()
-print(bf_inception)
+--print(bf_inception)
 torch.save('googlenet_bf_inception.t7', bf_inception)
 
 print('  ')
 print('softmax3')
 softmax3 = model:float()
-print(softmax3)
+--print(softmax3)
 torch.save('googlenet_softmax3.t7', softmax3)
 
 
 print('  ')
 print('softmax1')
 softmax1 = bf_sftmax1:add(af_softmax1):float()
-print(softmax1)
+--print(softmax1)
 torch.save('googlenet_softmax1.t7', softmax1)
 
 
 print('  ')
 print('softmax2')
 softmax2 = bf_sftmax2:add(af_softmax2):float()
-print(softmax2)
+--print(softmax2)
 torch.save('googlenet_softmax2.t7', softmax2)
 
 
 
 
---[[
-local size = 224
-local input = torch.randn(1, 3, size, size):fill(0)
-local output = model:forward(input)
-print(output:size())
-
-local test = true
-if test then
-  local meanAccum = 0
-  local count = 0
-  local batchSize = 32
-  local input = torch.Tensor(batchSize, 3, size, size)
-  local targets = torch.LongTensor(batchSize)
-  local dir = require 'pl.dir'
-  local gm = require 'graphicsmagick'
-
-  model:evaluate()
-
-  local mean = torch.Tensor(input:size())
-  mean[{{}, {1}, {}, {}}] = -104
-  mean[{{}, {2}, {}, {}}] = -117
-  mean[{{}, {3}, {}, {}}] = -123
-
-  local index = 1
-  --[[local dirs = dir.getdirectories('/data/imagenet/val/')
-  table.sort(dirs)
-  print(dirs)
-  for file in paths.iterfiles("/data/imagenet/val/") do
-      print(file)
-  end
-  local index = 1
-  print('---------')]]--
-  --[[for k1, v1 in ipairs(dirs) do
-    print(v1)
-      print('<<-------->>' )]]--
-    --[[k1 = 1
-    v1 = '/data/imagenet/val/'
-    for v2 in paths.iterfiles(v1) do
-      local img = gm.Image():load(paths.concat(v1, v2), 256, 256):crop(size, size, (256 - size) / 2, (256 - size) / 2)
-      print('<<-------->>' )
-      local imgTensor = img:toTensor('float', 'BGR', 'DHW')
-      input[{{index}, {}, {}, {}}]:copy(imgTensor)
-      targets[{{index}}] = k1
-      index = index + 1
-      print('-------->>' .. index)
-      if index == batchSize + 1 then
-        index = 1
-        k1 = k1+1
-        input:mul(255):add(mean)
-        local output = model:forward(input)
-        local _, order = output:sort(2)
-        local accuracy = targets:eq(order[{{}, {order:size()[2]}}]:reshape(order:size()[1]):long()):float():mean()
-        meanAccum = meanAccum + accuracy
-        count = count + 1
-        print(accuracy, meanAccum / count)
-      end
-    end
---  end
-end]]--
